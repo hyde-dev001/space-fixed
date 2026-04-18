dfe72d581c090b8ee29e3aeec8d54385e3f9aad3
hecate0001 <slyhand56@gmail.com>
2026-04-18 08:24:32 +0800

    'fix POS repair, and added duplicate checker in Add employee'

---
+++ public/build/assets/Form-CzMtU5t_.js
@@ -0,0 +1,1 @@
+import{j as m}from"./app-DYeZ8I8z.js";const e=({onSubmit:o,children:s,className:t})=>m.jsx("form",{onSubmit:r=>{r.preventDefault(),o(r)},className:` ${t}`,children:s});export{e as F};
