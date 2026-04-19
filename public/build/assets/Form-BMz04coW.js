f72f88df77bd300d00ccdd5f7105ed40f1fb31bd
hecate0001 <slyhand56@gmail.com>
2026-04-19 17:13:37 +0800

    'update: rebuild artifacts'

--- public/build/assets/Form-BMz04coW.js
+++
@@ -1,1 +0,0 @@
-import{j as m}from"./app-ZBnissZz.js";const e=({onSubmit:o,children:s,className:t})=>m.jsx("form",{onSubmit:r=>{r.preventDefault(),o(r)},className:` ${t}`,children:s});export{e as F};
