cfd8c25804cfe53b13fa96c92905ef860c01ec98
john daniel paragas <paragas.johndaniel@ncst.edu.ph>
2026-07-18 16:22:46 +0800

    fix: align employee model contracts

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Builder;
@@ -19,7 +20,7 @@ use Spatie\Activitylog\LogOptions;
  */
 class Employee extends Model
 {
-    use HasFactory, LogsActivity;
+    use HasFactory, LogsActivity, SoftDeletes;
 
     /**
      * The table associated with the model.
