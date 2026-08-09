41f5e760e8b92b654b4351dfe9429940e6a2d3a3
john daniel paragas <paragas.johndaniel@ncst.edu.ph>
2026-08-09 08:38:25 +0800

    build: refresh procurement assets

---
+++ public/build/assets/Form-DV6j1cLn.js
@@ -0,0 +1,1 @@
+import{j as m}from"./app-_8rP_vYq.js";const e=({onSubmit:o,children:s,className:t})=>m.jsx("form",{onSubmit:r=>{r.preventDefault(),o(r)},className:` ${t}`,children:s});export{e as F};
