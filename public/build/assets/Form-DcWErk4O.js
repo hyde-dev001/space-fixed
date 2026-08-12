073c14f671606f9d108a9892cd2c8cfe41b997f5
Franszine Miguel Bato <fmybato@gmail.com>
2026-08-12 21:19:52 +0800

    feat: move shipment delivery details to modal

---
+++ public/build/assets/Form-DcWErk4O.js
@@ -0,0 +1,5 @@
+<<<<<<<< HEAD:public/build/assets/Form-Simx3ryC.js
+import{j as m}from"./app-BzG1iCvU.js";const e=({onSubmit:o,children:s,className:t})=>m.jsx("form",{onSubmit:r=>{r.preventDefault(),o(r)},className:` ${t}`,children:s});export{e as F};
+========
+import{j as m}from"./app-DVFUWuZy.js";const e=({onSubmit:o,children:s,className:t})=>m.jsx("form",{onSubmit:r=>{r.preventDefault(),o(r)},className:` ${t}`,children:s});export{e as F};
+>>>>>>>> ebe911608 (feat: move shipment delivery details to modal):public/build/assets/Form-DcWErk4O.js
---
+++ public/build/assets/backNavigation-B_vq25K5.js
@@ -0,0 +1,5 @@
+<<<<<<<< HEAD:public/build/assets/backNavigation-DQIaytwh.js
+import{r as e}from"./app-BzG1iCvU.js";const i=()=>{if(typeof window>"u"||!document.referrer)return!1;try{return new URL(document.referrer).origin===window.location.origin}catch{return!1}},t=r=>{if(typeof window>"u"){e.visit(r);return}if(window.history.length>1&&i()){window.history.back();return}e.visit(r)};export{t as n};
+========
+import{r as e}from"./app-DVFUWuZy.js";const i=()=>{if(typeof window>"u"||!document.referrer)return!1;try{return new URL(document.referrer).origin===window.location.origin}catch{return!1}},t=r=>{if(typeof window>"u"){e.visit(r);return}if(window.history.length>1&&i()){window.history.back();return}e.visit(r)};export{t as n};
+>>>>>>>> ebe911608 (feat: move shipment delivery details to modal):public/build/assets/backNavigation-B_vq25K5.js
