2d60939d33bd3669c3d9f8c989279fd33bfc63a6
Franszine Miguel Bato <fmybato@gmail.com>
2026-08-12 19:09:25 +0800

    fix: restore landing hero animation

---
+++ public/build/assets/Form-CJHUTUzy.js
@@ -0,0 +1,1 @@
+import{j as m}from"./app-DIVfk9eh.js";const e=({onSubmit:o,children:s,className:t})=>m.jsx("form",{onSubmit:r=>{r.preventDefault(),o(r)},className:` ${t}`,children:s});export{e as F};
