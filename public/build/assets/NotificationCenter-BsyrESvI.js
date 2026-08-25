96ad7293eebc0d025ede6f2d6504afce3ec6dbca
Franszine Miguel Bato <fmybato@gmail.com>
2026-08-25 21:41:03 +0800

    fix: maximize compact account menu space

---
+++ public/build/assets/NotificationCenter-BsyrESvI.js
@@ -0,0 +1,1 @@
+import{N as o}from"./NotificationCenter-BaYXtMFi.js";import"./app-CEMyuQ0K.js";import"./vendor-apexcharts-C5L_vV5G.js";/* empty css            */import"./useNotifications-C8OLbLsq.js";import"./useQuery-D5ba5xbW.js";import"./useMutation-B6x9Pbrd.js";import"./XMarkIcon-DHhaSAmC.js";export{o as default};
