fddf83bcbbd121b4307ef2fbeed5e192e1929fdf
Franszine Miguel Bato <fmybato@gmail.com>
2026-08-26 19:01:48 +0800

    Revert "fix: restore transparent icon controls"
    This reverts commit b8a11305bd7f5250565bbcd3a38c6aba764b157b.

--- public/build/assets/Form-lhQ4XBBf.js
+++
@@ -1,1 +0,0 @@
-import{j as m}from"./app-CCnxsP8V.js";const e=({onSubmit:o,children:s,className:t})=>m.jsx("form",{onSubmit:r=>{r.preventDefault(),o(r)},className:` ${t}`,children:s});export{e as F};
---
+++ public/build/assets/NotificationCenter-CVxwhfSt.js
@@ -0,0 +1,1 @@
+import{N as o}from"./NotificationCenter-VDenK4KZ.js";import"./app-BtBHwLfl.js";import"./vendor-apexcharts-C5L_vV5G.js";/* empty css            */import"./useNotifications-Bba9nbXk.js";import"./useQuery-HYNWBLX1.js";import"./useMutation-DIswNJq1.js";import"./XMarkIcon-DHhaSAmC.js";export{o as default};
