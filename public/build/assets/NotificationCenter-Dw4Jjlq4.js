c096d96819560d0c6a6420ab4a739029486bf281
Franszine Miguel Bato <fmybato@gmail.com>
2026-08-29 13:40:39 +0800

    style: move registration authority to document switch

---
+++ public/build/assets/NotificationCenter-Dw4Jjlq4.js
@@ -0,0 +1,1 @@
+import{N as o}from"./NotificationCenter-FQu76DAu.js";import"./app-rtrk7UiL.js";import"./vendor-apexcharts-DPaa2SoU.js";/* empty css            */import"./useNotifications-vF7PEwfY.js";import"./useQuery-DuMjZynn.js";import"./useMutation-Cy2R3AES.js";import"./resolveNotificationActionUrl-Cbpqk05R.js";import"./XMarkIcon-C6pjG1Mv.js";export{o as default};
