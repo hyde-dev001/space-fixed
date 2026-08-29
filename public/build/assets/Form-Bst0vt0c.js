0bfc3b1e7f259e992170134536a6db4c0c0df52f
Franszine Miguel Bato <fmybato@gmail.com>
2026-08-29 09:50:41 +0800

    build: refresh production assets after rebase

--- public/build/assets/Form-Bst0vt0c.js
+++
@@ -1,1 +0,0 @@
-import{j as m}from"./app-Vro_1qQc.js";const e=({onSubmit:o,children:s,className:t})=>m.jsx("form",{onSubmit:r=>{r.preventDefault(),o(r)},className:` ${t}`,children:s});export{e as F};
