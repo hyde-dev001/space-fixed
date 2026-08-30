4635c0ac9408151ed03649cdfd3deb09ee25f197
Franszine Miguel Bato <fmybato@gmail.com>
2026-08-30 15:32:35 +0800

    build: refresh landing footer assets

--- public/build/assets/Form-C4h2tBK1.js
+++
@@ -1,1 +0,0 @@
-import{j as m}from"./app-BE5pfbdC.js";const e=({onSubmit:o,children:s,className:t})=>m.jsx("form",{onSubmit:r=>{r.preventDefault(),o(r)},className:` ${t}`,children:s});export{e as F};
--- public/build/assets/NotificationCenter-BIjyXqi7.js
+++
@@ -1,1 +0,0 @@
-import{N as o}from"./NotificationCenter-CvoWi7dz.js";import"./app-BE5pfbdC.js";import"./vendor-apexcharts-DPaa2SoU.js";/* empty css            */import"./useNotifications-DI5k9ro1.js";import"./useQuery-C_axxLEp.js";import"./useMutation-B7HY5lXN.js";import"./resolveNotificationActionUrl-Cbpqk05R.js";import"./XMarkIcon-C6pjG1Mv.js";export{o as default};
