6f018fa92109bb05e9cd16d1f721f998aea3a03a
Franszine Miguel Bato <fmybato@gmail.com>
2026-09-03 00:03:04 +0800

    fix: skip desktop app loader

--- public/build/assets/NotificationCenter-B8iohDTP.js
+++
@@ -1,1 +0,0 @@
-import{N as o}from"./NotificationCenter-CrOtiI3E.js";import"./app-Rxtr0mFB.js";import"./vendor-apexcharts-DPaa2SoU.js";/* empty css            */import"./useNotifications-8faP2WHx.js";import"./useQuery-BCKmzF5T.js";import"./useMutation-CUAWH-fj.js";import"./resolveNotificationActionUrl-Cbpqk05R.js";import"./XMarkIcon-C6pjG1Mv.js";export{o as default};
