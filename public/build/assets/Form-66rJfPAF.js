1ddc6f712f1ad7214d2f45e4516cf0a405c5a36c
Franszine Miguel Bato <fmybato@gmail.com>
2026-09-03 15:13:59 +0800

    chore: refresh frontend build

--- public/build/assets/Form-66rJfPAF.js
+++
@@ -1,1 +0,0 @@
-import{j as m}from"./app-g3cQLPsg.js";const e=({onSubmit:o,children:s,className:t})=>m.jsx("form",{onSubmit:r=>{r.preventDefault(),o(r)},className:` ${t}`,children:s});export{e as F};
--- public/build/assets/NotificationCenter-Bkc-InsQ.js
+++
@@ -1,1 +0,0 @@
-import{N as o}from"./NotificationCenter-BJZbniP5.js";import"./app-g3cQLPsg.js";import"./vendor-apexcharts-DPaa2SoU.js";/* empty css            */import"./useNotifications-5P2xxGD7.js";import"./useQuery-CIjaQ1t9.js";import"./useMutation-a3qEMq21.js";import"./resolveNotificationActionUrl-Cbpqk05R.js";import"./XMarkIcon-C6pjG1Mv.js";export{o as default};
