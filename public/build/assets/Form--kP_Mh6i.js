8a3225d93a0f9d701640277e394aad853d65d18e
Franszine Miguel Bato <fmybato@gmail.com>
2026-09-08 15:52:54 +0800

    fix: refine payroll release layout

---
+++ public/build/assets/Form--kP_Mh6i.js
@@ -0,0 +1,1 @@
+import{j as m}from"./app-uHl1533Y.js";const e=({onSubmit:o,children:s,className:t})=>m.jsx("form",{onSubmit:r=>{r.preventDefault(),o(r)},className:` ${t}`,children:s});export{e as F};
