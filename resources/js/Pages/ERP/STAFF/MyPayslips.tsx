6863ca918057196ad8c4ea7cb93de38646fac438
Franszine Miguel Bato <fmybato@gmail.com>
2026-09-08 17:04:27 +0800

    fix: make my payslips background white

--- resources/js/Pages/ERP/STAFF/MyPayslips.tsx
+++ resources/js/Pages/ERP/STAFF/MyPayslips.tsx
@@ -442,7 +442,7 @@ export default function MyPayslips() {
 		<AppLayoutERP>
 			<Head title="My Payslips" />
 
-			<div className="min-h-screen bg-gray-50 dark:bg-gray-950 px-4 py-8">
+			<div className="min-h-screen bg-white dark:bg-gray-950 px-4 py-8">
 				<div className="max-w-3xl mx-auto">
 
 					<h1 className="sr-only">My Payslips</h1>
