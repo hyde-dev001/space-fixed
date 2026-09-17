769176a5246258ba848bb91025754ccb7f12adae
Franszine Miguel Bato <fmybato@gmail.com>
2026-09-17 23:39:06 +0800

    fix: format audit actor roles

---
+++ public/build/assets/Form-BV1h2fpf.js
@@ -0,0 +1,1 @@
+import{j as m}from"./app-BinZdO_d.js";const e=({onSubmit:o,children:s,className:t})=>m.jsx("form",{onSubmit:r=>{r.preventDefault(),o(r)},className:` ${t}`,children:s});export{e as F};
