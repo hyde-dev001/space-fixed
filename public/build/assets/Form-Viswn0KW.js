40b44e9e7133f32f8e70cb5086bae0f31bf95581
Franszine Miguel Bato <fmybato@gmail.com>
2026-09-20 16:14:46 +0800

    fix: allow intake tracking at ready for pickup

---
+++ public/build/assets/Form-Viswn0KW.js
@@ -0,0 +1,1 @@
+import{j as m}from"./app-C7cY4o3d.js";const e=({onSubmit:o,children:s,className:t})=>m.jsx("form",{onSubmit:r=>{r.preventDefault(),o(r)},className:` ${t}`,children:s});export{e as F};
