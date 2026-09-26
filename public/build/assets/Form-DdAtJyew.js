46dc34f5e650a824eb3723892cddb1b8845dcb37
Franszine Miguel Bato <fmybato@gmail.com>
2026-09-26 16:36:36 +0800

    fix: resize employee details modal

--- public/build/assets/Form-DdAtJyew.js
+++
@@ -1,1 +0,0 @@
-import{j as m}from"./app-DmT4U9Gu.js";const e=({onSubmit:o,children:s,className:t})=>m.jsx("form",{onSubmit:r=>{r.preventDefault(),o(r)},className:` ${t}`,children:s});export{e as F};
